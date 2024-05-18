7d6f48b29feb3e9289676b81e9efec6e73977982
DavidFonsecaNS <carlos.fonseca@northwayservices.com>
2024-05-18 15:15:05 -0500

    MovimientoBD

--- AutoBanca.BD/MovimientoBD.cs
+++ AutoBanca.BD/MovimientoBD.cs
@@ -40,8 +40,61 @@ namespace AutoBanca.BD
         public string Mov_descripcion { get; set; }
         public int Cue_id { get; set; }
 
-        
 
+        // Procemiento de Movimiento_Insert
+        public void Movimiento_Insert(DateTime mov_fecha, decimal mov_monto, string mov_tipo, string mov_descripcion, int cue_id)
+        {
+            try
+            {
+                BD.Movimiento_Insert(mov_fecha, mov_monto, mov_tipo, mov_descripcion, cue_id);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
 
+        // Procemiento de Movimiento_List
+        public List<Movimiento_List_Result> Movimiento_List(int cue_id)
+        {
+            try
+            {
+                var Movimiento = BD.Movimiento_List(cue_id).ToList();
+                return Movimiento;
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+
+        // Procedimiento de Movimiento_Delete
+        public void Movimiento_Delete(int mov_id)
+        {
+            try
+            {
+                BD.Movimiento_Delete(mov_id);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+
+        // Procedimiento de Movimiento_Update
+        public void Movimiento_Update(int mov_id, DateTime mov_fecha, decimal mov_monto, string mov_tipo, string mov_descripcion, int cue_id)
+        {
+            try
+            {
+                BD.Movimiento_Update(mov_id, mov_fecha, mov_monto, mov_tipo, mov_descripcion, cue_id);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
     }
 }
